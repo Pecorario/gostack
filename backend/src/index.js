cda8f6fb707dd2884bbcb3c9dab09782cf0b0b09
Taynara Pecorario <taynara_rp22@hotmail.com>
2020-08-17 18:08:00 -0300

    aplicacao parametros

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { uuid } = require('uuidv4'); // universal unique id
 
 const app = express();
 
@@ -20,48 +21,61 @@ app.use(express.json());
     Request Body: conteúdo na hora de criar ou editar um recurso (JSON)
 */
 
+const projects = [];
+
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query;
+  const { title } = request.query;
 
-  console.log(title);
-  console.log(owner);
+  const results = title //vê o que foi enviado no title
+    ? projects.filter(project => project.title.includes(title)) //se foi preenchido pelo usuario, realiza uma busca dentro de cada objeto project e verifica se dentro do atributo title dele, existe a palavra que foi preenchida no campo de busca
+    : projects; //se for enviado em branco, retorno todos os projetcts
 
-  return  response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+  return  response.json(results);
 });
 
 app.post('/projects', (request, response) => {
   const { title, owner } = request.body;
 
-  console.log(title);
-  console.log(owner);
+  const project = { id: uuid(), title, owner };
 
-  return  response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  projects.push(project); // add o objeto project no final do array projects
+
+  return  response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
   const { id } = request.params;
+  const { title, owner } = request.body;
 
-  console.log(id);
-  
-  return  response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const projectIndex = projects.findIndex(project => project.id === id); //procura em cada objeto do array projects um project cujo id coincida com o id passado e acha a posição dele dentro desse vetor
+
+  if (projectIndex < 0) { //se o objeto nao for encontrado
+    return response.status(400).json({ error: 'Project not found.' }); //seto o status 400 que é um codigo genérico para erro no back-end
+  }
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project; //dentro do array de projects, procuro a posição do projectIndex e substituo o objeto daquela posição pelo objeto q acabei de criar
+
+  return  response.json(project); //retorno o objeto atualizado
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return  response.json([
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const { id } = request.params;
+
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(400).json({ error: 'Project not found.' });
+  }
+
+  projects.splice(projectIndex, 1); //splice: método para tirar uma informação de um array (indice que quero remover, quantas posições quero remover a partir desse indice; como quero remover apenas a informação contida nesse índice, passo 1)
+
+  return  response.status(204).send(); //status 204 para dizer que a resposta nao tem conteudo
 });
 
 app.listen(3333, () => {
