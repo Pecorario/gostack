f770533431f23e8d5ba1a3193ee6cb607f860041
Taynara Pecorario <taynara_rp22@hotmail.com>
2020-08-24 00:31:26 -0300

    conectando frontend com backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './App.css';
 
 import Header from './components/Header';
 
@@ -11,16 +14,21 @@ import Header from './components/Header';
 */
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
 
   // useState retorna um array com 2 posições:
   // 2. Variável com o seu valor inicial
   // 3. Função para atualizarmos esse valor
 
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []); //recebe 2 parametros: 1. qual função quero disparar; 2. quando quero disparar essa função?
+  
   function handleAddProject() {
     setProjects([...projects, `Novo projeto ${Date.now()}`]); //gera um novo array; ...projects vai percorrer todo meu array de projects e copiar cada projeto para dentro do meu array
 
-    console.log(projects);
   }
 
   return (
@@ -28,7 +36,7 @@ function App() {
       <Header title="Projects" />
        
       <ul> 
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
 
         <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
       </ul>
